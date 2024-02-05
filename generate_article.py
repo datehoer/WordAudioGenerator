16e17a9716fa30a78f68ba9893eb8d2d17457625
cxk <datehoer@gmail.com>
2024-02-05 21:02:38 +0800

    添加新的音频生成方式，将单词出现次数为0的重新生成一份文章

--- generate_article.py
+++ generate_article.py
@@ -7,6 +7,7 @@ import pandas as pd
 from translate import google_translate, YoudaoTranslater
 from config import *
 import sqlite3
+import pyttsx3
 # import epitran
 
 
@@ -26,22 +27,28 @@ def translate_words(words, title, word_count):
     columns = ['No.', 'Word', 'Meaning', 'Pronunciation', 'Count']
     word = []
     batch_data = []
-    for i, item in enumerate(words.split(" "), start=1):
-        word_meaning = get_word_meaning(item)
-        word_phonetic = get_word_phonetic(item)
-        if not word_meaning:
-            word_meaning = google_translate(item, toLan="zh-CN", proxy=proxy)
-            word_data = get_word(item)
-            if word_data and word_data['meaning'] and word_data['phonetic']:
-                word_meaning = word_data['meaning']
-                word_phonetic = word_data['phonetic']
-                batch_data.append((item, word_phonetic, word_meaning))
-                # cursor.execute("INSERT OR REPLACE INTO words VALUES (?, ?, ?)", (item, word_phonetic, word_meaning))
-        word.append([i, item, word_meaning, word_phonetic, word_count[item]])
-    if batch_data:
+    for i, item in enumerate(words, start=1):
+        try:
+            word_meaning = get_word_meaning(item)
+            word_phonetic = get_word_phonetic(item)
+            if not word_meaning:
+                word_meaning = google_translate(item, toLan="zh-CN", proxy=proxy)
+                word_data = get_word(item)
+                if word_data and word_data['meaning'] and word_data['phonetic']:
+                    word_meaning = word_data['meaning']
+                    word_phonetic = word_data['phonetic']
+                    batch_data.append((item, word_phonetic, word_meaning))
+                    # cursor.execute("INSERT OR REPLACE INTO words VALUES (?, ?, ?)", (item, word_phonetic, word_meaning))
+            word.append([i, item, word_meaning, word_phonetic, word_count[item]])
+        except Exception as e:
+            logging.error(f"Translate Error: {e}")
+    if len(batch_data) > 0:
         execute_sql(batch_data)
-    df = pd.DataFrame(word, columns=columns)
-    df.to_excel('data/' + title.split(': ')[-1].replace(" ", "_") + '_output.xlsx', index=False)
+    try:
+        df = pd.DataFrame(word, columns=columns)
+        df.to_excel('data/' + title.split(': ')[-1].replace(" ", "_") + '_output.xlsx', index=False)
+    except Exception as e:
+        logging.error(f"Write Excel Error: {e}")
 
 
 def translate_words_youdao(words, title, word_count):
@@ -49,23 +56,29 @@ def translate_words_youdao(words, title, word_count):
     columns = ['No.', 'Word', 'Meaning', 'Pronunciation', 'Count']
     word = []
     batch_data = []
-    for i, item in enumerate(words.split(" "), start=1):
-        word_meaning = get_word_meaning(item)
-        word_phonetic = get_word_phonetic(item)
-        if not word_meaning:
-            word_meaning = translate_youdao.translate(item, toLan="zh-CHS")
-            if word_meaning and word_meaning.get("code") == 0:
-                dict_result = word_meaning.get("dictResult")
-                ec = dict_result.get("ec")
-                ec_word = ec.get("word")
-                word_phonetic = "[" + ec_word.get("usphone") + "]"
-                word_meaning = word_meaning.get("translateResult")[0].get("tgt")
-                batch_data.append((item, word_phonetic, word_meaning))
-        word.append([i, item, word_meaning, word_phonetic, word_count[item]])
-    if batch_data:
+    for i, item in enumerate(words, start=1):
+        try:
+            word_meaning = get_word_meaning(item)
+            word_phonetic = get_word_phonetic(item)
+            if not word_meaning:
+                word_meaning = translate_youdao.translate(item, toLan="zh-CHS")
+                if word_meaning and word_meaning.get("code") == 0:
+                    dict_result = word_meaning.get("dictResult")
+                    ec = dict_result.get("ec")
+                    ec_word = ec.get("word")
+                    word_phonetic = "[" + ec_word.get("usphone") + "]"
+                    word_meaning = word_meaning.get("translateResult")[0].get("tgt")
+                    batch_data.append((item, word_phonetic, word_meaning))
+            word.append([i, item, word_meaning, word_phonetic, word_count[item]])
+        except Exception as e:
+            logging.error(f"Translate Error: {e}")
+    if len(batch_data) > 0:
         execute_sql(batch_data)
-    df = pd.DataFrame(word, columns=columns)
-    df.to_excel('data/' + title.split(': ')[-1].replace(" ", "_") + '_output.xlsx', index=False)
+    try:
+        df = pd.DataFrame(word, columns=columns)
+        df.to_excel('data/' + title.split(': ')[-1].replace(" ", "_") + '_output.xlsx', index=False)
+    except Exception as e:
+        logging.error(f"Write Excel Error: {e}")
 
 
 def request(url, data, headers):
@@ -127,6 +140,14 @@ def get_word_meaning(word):
     return part_of_speech
 
 
+def generate_and_save_audio_microsoft(content, title):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 125)
+    engine.setProperty('volume', 1.0)
+    engine.save_to_file(content, "data/{}.mp3".format(title.split(': ')[-1]))
+    engine.runAndWait()
+
+
 def generate_and_save_audio(content, title):
     url = openai_tts_url.replace("api.openai.com", token_url)
     data = openai_tts_data
@@ -146,8 +167,41 @@ def save_text_file(content, title):
 
 
 def count_words_in_article(word_list, article):
-    word = {word: len(re.findall(word, article, re.IGNORECASE)) for word in word_list}
-    return word
+    word_count_plus = {}
+    word_count_zero = {}
+    for word in word_list:
+        count = len(re.findall(word, article, re.IGNORECASE))
+        if count > 0:
+            word_count_plus[word] = count
+        else:
+            word_count_zero[word] = count
+    return word_count_plus, word_count_zero
+
+
+def generate_article(words):
+    messages = [
+        {"role": "user",
+         "content": "请通过下面的单词写一篇英文文章必须包含所有的单词，语句通顺意思明确,返回的数据为json格式数据，包含title,content。{}".format(words)
+         }
+    ]
+    chat_data = openai_text_data.copy()
+    chat_data['messages'] = messages
+    url = openai_text_url.replace("api.openai.com", token_url)
+    try:
+        res = request(url, headers=openai_header, data=chat_data)
+        if res is None:
+            return None, None
+        res_json = res.json()
+        content_json = res_json['choices'][0]['message']['content']
+        content_json = json.loads(content_json.replace("```json", "").replace("```", "").replace("\n", ""))
+        content = content_json['content']
+        title = content_json['title']
+        generate_and_save_audio(content, title)
+        save_text_file(content, title)
+        return content, title
+    except Exception as e:
+        logging.error(f"Generate Article Error: {e}")
+        return None, None
 
 
 def main():
@@ -155,26 +209,21 @@ def main():
         with open("words.txt", "r") as f:
             words = f.read().split("\n")
         words = replace_multiple_spaces_with_single_space(" ".join(words))
-
-        messages = [
-            {"role": "user", "content": "请通过下面的单词写一篇英文文章必须包含所有的单词，语句通顺意思明确,返回的数据为json格式数据，包含title,content。{}".format(words)}
-        ]
-
-        chat_data = openai_text_data.copy()
-        chat_data['messages'] = messages
-        url = openai_text_url.replace("api.openai.com", token_url)
-        res = request(url, headers=openai_header, data=chat_data)
-        if res is None:
+        content, title = generate_article(words)
+        if content is None:
             return
-        res_json = res.json()
-        content_json = res_json['choices'][0]['message']['content']
-        content_json = json.loads(content_json.replace("```json", "").replace("```", "").replace("\n", ""))
-        content = content_json['content']
-        title = content_json['title']
-        word_count = count_words_in_article(words.split(" "), content)
-        translate_words(words, title, word_count)
-        generate_and_save_audio(content, title)
-        save_text_file(content, title)
+        words = words.split(" ")
+        word_count_plus, word_count_zero = count_words_in_article(words, content)
+        translate_words(list(word_count_plus), title, word_count_plus)
+        if len(word_count_zero) > 0:
+            words = list(word_count_zero)
+            content, title = generate_article(" ".join(words))
+            if content is None:
+                return
+            word_count_plus, word_count_zero = count_words_in_article(words, content)
+            word_count = {**word_count_plus, **word_count_zero}
+            translate_words(words, title, word_count)
+            return
     except Exception as e:
         logging.error(f"Main Error: {e}")
 
